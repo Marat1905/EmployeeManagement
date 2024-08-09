f6f3801f265d75a72296560f5d8814a6d68754f8
Marat <milya010790@mail.ru>
2024-08-09 15:38:57 +0500

    Добавлена модель представления

---
+++ EmployeeManagement/ViewModels/EmployeesViewModel.cs
@@ -0,0 +1,18 @@
+﻿using EmployeeManagement.Data.Repository;
+using EmployeeManagement.Models;
+using System.Collections.ObjectModel;
+
+namespace EmployeeManagement.ViewModels
+{
+    public class EmployeesViewModel
+    {
+        private readonly EmployeeRepository _employeeRepository;
+
+        public EmployeesViewModel()
+        {
+            _employeeRepository = new EmployeeRepository();
+        }
+
+        public ObservableCollection<Employee> Employees { get { return new ObservableCollection<Employee>(_employeeRepository.GetAll()); } }
+    }
+}
