21fd96f1c6eb5561a089aab8185220e3fde1619c
Marat <milya010790@mail.ru>
2024-08-09 14:02:06 +0500

    Добавьте файлы проекта.

---
+++ EmployeeManagement/Views/Windows/EmploeesView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace EmployeeManagement.Views.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для EmploeesView.xaml
+    /// </summary>
+    public partial class EmploeesView : Window
+    {
+        public EmploeesView()
+        {
+            InitializeComponent();
+        }
+    }
+}
