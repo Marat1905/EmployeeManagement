4289e72248694fa52bb9450194d43489a380af26
Marat <milya010790@mail.ru>
2024-08-09 16:07:23 +0500

    добавили сообщения

--- EmployeeManagement/Views/Windows/EmploeesView.xaml.cs
+++ EmployeeManagement/Views/Windows/EmploeesView.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using EmployeeManagement.Models;
+using System.Windows;
+using System.Windows.Controls;
 
 namespace EmployeeManagement.Views.Windows
 {
@@ -10,6 +12,16 @@ namespace EmployeeManagement.Views.Windows
         public EmploeesView()
         {
             InitializeComponent();
+        }
+
+        private void ListView_PreviewMouseDoubleClick(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            var item = (sender as ListView).SelectedItem;
+            if(item is null)
+                return;
+
+            var employee = item as Employee;
+            MessageBox.Show(employee.FirstName);
         }
     }
 }
